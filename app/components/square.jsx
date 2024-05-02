774116829bec2c914b30fc66a98fb5c021e7c395
Ben Gilbert <alicornderpy@gmail.com>
2024-05-02 11:41:45 -0700

    made dragging sorta work

--- app/components/square.jsx
+++ app/components/square.jsx
@@ -2,31 +2,47 @@ import { useRef } from 'react';
 import { useDrag, useDrop } from 'react-dnd';
 import styles from '@/app/page.module.css';
 
-function swapSquares(chartData, setChartData, index1, index2) {
-    const temp = chartData.albumList[index1];
-    chartData.albumList[index1] = chartData.albumList[index2];
-    chartData.albumList[index2] = temp;
-    setChartData({...chartData});
-}
-
 export default function Square({chartData, setChartData, index, size, padding}) {
+    const swapSquares = (index1, index2) => {
+        console.log(chartData.albumList[index1]);
+        console.log(chartData.albumList[index2]);
+        const temp = chartData.albumList[index1];
+        chartData.albumList[index1] = chartData.albumList[index2];
+        chartData.albumList[index2] = temp;
+        setChartData({...chartData});
+    };
+    
     const ref = useRef(null);
     const [{ canDrop, isOver }, drop] = useDrop(() => ({
         accept: ['album', 'square'],
         drop: (item, monitor) => {
-
-            chartData.albumList[index] = item;
-            setChartData({...chartData});
+            if (monitor.getItemType() === 'album') {
+                chartData.albumList[index] = item;
+                setChartData({...chartData});
+            } else if (albumData == undefined) {
+                swapSquares(index, item.index);
+            }
         },
         collect: (monitor) => ({
             isOver: monitor.isOver(),
             canDrop: monitor.canDrop()
         }),
         hover(item, monitor) {
-            if (!ref.current) {
+            if (albumData == undefined || !ref.current || monitor.getItemType() === 'album') {
                 return;
             }
             const dragIndex = item.index;
+            const hoverIndex = index;
+            if (dragIndex === hoverIndex) {
+                return;
+            }
+            const hoverBoundingRect = ref.current?.getBoundingClientRect();
+            const clientOffset = monitor.getClientOffset();
+            if ((hoverBoundingRect.bottom < clientOffset.y < hoverBoundingRect.top)
+            && (hoverBoundingRect.left < clientOffset.x < hoverBoundingRect.right)) {
+                swapSquares(dragIndex, hoverIndex);
+                item.index = hoverIndex;
+            }
         }
     }), [chartData]);
 
@@ -35,17 +51,19 @@ export default function Square({chartData, setChartData, index, size, padding}) 
     const [{isDragging}, drag] = useDrag ({
         type: 'square',
         item: {albumData: albumData, index: index},
+        canDrag: albumData != undefined,
         collect: (monitor) => ({
             isDragging: monitor.isDragging()
         })
-    }, []);
+    });
 
 
     const isActive = canDrop && isOver;
     let backgroundColor = 'white';
     if (isActive) {
         backgroundColor = 'darkgreen';
-    } else if (isDragging) {
+    }
+    if (isDragging) {
         backgroundColor = '#555555';
     }
     drag(drop(ref));
@@ -61,7 +79,7 @@ export default function Square({chartData, setChartData, index, size, padding}) 
             className = { styles.square } 
             data-handler-id={index}
         >
-            {albumData != undefined && (
+            {(albumData != undefined && !isDragging) && (
                 <img
                     style= {{margin:padding}}
                     height={size} 
